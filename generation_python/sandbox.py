d1ea649189ef6b15a37259086cb8f2b9e31ab41a
gleboshur <gleboshur@gmail.com>
2026-09-01 20:50:15 +0500

    upd

--- generation_python/sandbox.py
+++ generation_python/sandbox.py
@@ -1,13 +1,19 @@
-name = input()
-flag = False
+price = int(input())
 counter = 0
-while name != "Левон":
-    if name == "Александра":
-        flag = True
-    if flag:
-        counter += 1
-    name = input()
-print(counter - 1)
+while price >= 25:
+    price -= 25
+    counter += 1
+while price >= 10:
+    price -= 10
+    counter += 1    
+while price >= 5:
+    price -= 5
+    counter += 1
+while price >= 1:
+    price -= 1
+    counter += 1
+print(counter)
+
 
 
 
