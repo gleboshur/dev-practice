d630d94098a7e8325bf3f009e611090d54c7f2d3
gleboshur <gleboshur@gmail.com>
2026-09-02 22:14:24 +0500

    upd

--- generation_python/sandbox.py
+++ generation_python/sandbox.py
@@ -1,20 +1,7 @@
-price = int(input())
-counter = 0
-while price >= 25:
-    price -= 25
-    counter += 1
-while price >= 10:
-    price -= 10
-    counter += 1    
-while price >= 5:
-    price -= 5
-    counter += 1
-while price >= 1:
-    price -= 1
-    counter += 1
-print(counter)
-
-
-
-
-
+n = int(input())
+product = 1
+while n > 0:
+    digit = n % 10
+    product = product * digit
+    n //= 10
+print(product)
