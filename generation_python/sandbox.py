67080e5922368fb45011041ea41fc0c86d203157
gleboshur <gleboshur@gmail.com>
2026-09-03 20:15:02 +0500

    upd

--- generation_python/sandbox.py
+++ generation_python/sandbox.py
@@ -1,7 +1,9 @@
 n = int(input())
-product = 1
-while n > 0:
-    digit = n % 10
-    product = product * digit
-    n //= 10
-print(product)
+for i in range(1, n + 1):
+    for j in range(i):
+        print(i, end="")
+    print()
+
+
+
+
