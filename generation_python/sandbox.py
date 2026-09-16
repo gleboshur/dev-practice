f25bbd0699b25b4c86154e5c631bb3207e3eac88
gleboshur <gleboshur@gmail.com>
2026-09-16 18:01:37 +0500

    upd

--- generation_python/sandbox.py
+++ generation_python/sandbox.py
@@ -1,14 +1,2 @@
-s = input()
-if (
-    9 <= len(s) <= 10
-    and (s[0]+s[4:6]).isupper()
-    and (s[0]+s[4:6]).isalpha()
-    and s[6] == '_'
-    and s[0] in 'АВЕКМНОРСТУХ'
-    and s[4] in 'АВЕКМНОРСТУХ'
-    and s[5] in 'АВЕКМНОРСТУХ'
-    and (s[1:4]+s[7:]).isdigit()
-):
-    print('YES')
-else:
-    print('NO')
+s = list(input())
+print(s[::2])
