f119f77f2934f74e4e21fc163520a3c4cc99275e
gleboshur <gleboshur@gmail.com>
2026-09-19 16:38:23 +0500

    upd

--- generation_python/sandbox.py
+++ generation_python/sandbox.py
@@ -1,3 +1,8 @@
-s = input()
-seq = s.split("\\")
-print(*seq, sep="\n")
+num = input().split('-')
+if len(num) == 3 or (len(num) == 4 and num[0] == '7'):
+    if len(num[-3]) == 3 and len(num[-2]) == 3 and len(num[-1]) == 4  and num[-3].isdigit() and num[-2].isdigit() and num[-1].isdigit():
+        print('YES')
+    else:
+        print('NO')
+else:
+    print('NO')
