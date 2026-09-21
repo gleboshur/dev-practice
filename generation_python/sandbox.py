d1bea68cedb33c20be93fa3f26a68e032e2b1aff
gleboshur <gleboshur@gmail.com>
2026-09-21 14:59:51 +0500

    upd

--- generation_python/sandbox.py
+++ generation_python/sandbox.py
@@ -1,11 +1,5 @@
-def print_symbol_counts(s):
-    chars = list(s.lower())
-    chars.sort()
-    res = ''
-    for el in chars:
-        if el not in res:
-            res += el
-            print(f'{el}: {chars.count(el)}')
-            
-s = input()
-print_symbol_counts(s)
+def convert_to_miles(km):
+    return km * 0.6214
+
+num = int(input())
+print(convert_to_miles(num))
